de0c361066f22c07b0d90f31066508f6c200f174
Andrew Marxer <andy.marxer@yahoo.com>
2021-10-28 15:33:58 -0500

    add

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import bcrypt
 import flask_bcrypt
-from flask import render_template, url_for, redirect, flash
+from flask import render_template, url_for, redirect, flash, session
+from flask_login import LoginManager, login_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
@@ -11,11 +13,14 @@ from __init__ import create_app
 from models import *
 
 app = create_app('dev')
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 debug = True
 
 
 class create_user_form(FlaskForm):
+
     first_name = StringField(
         validators=[InputRequired()],
         render_kw={'class': 'form-control form-control-lg', 'placeholder': 'First Name'})
@@ -28,6 +33,10 @@ class create_user_form(FlaskForm):
     business_name = StringField(
         validators=[InputRequired()],
         render_kw={'class': 'form-control form-control-lg','placeholder': 'Business Name'})
+    business_type= SelectField(
+        validators=[InputRequired()],
+        render_kw={'class': 'form-control form control-lg'}
+    )
     password = PasswordField(
         validators=[InputRequired(), Length(min=6)],
         render_kw={'class': 'form-control form-control-lg','placeholder': 'Password'})
@@ -35,12 +44,30 @@ class create_user_form(FlaskForm):
         "Sign Up",
         render_kw={'class': 'register_input'})
 
-    def validate_username(self, email):
-        user = UserInfo.query.filter_by(email=email.data).first()
+    def validate_email(self, email):
+        user = Business.query.filter_by(email=email.data).first()
+        debug=True
         if user:
             raise ValidationError(
                 "Username already exists, please choose a different one or login if you already have an account")
 
+class loginForm(FlaskForm):
+    email = StringField(
+        validators=[InputRequired()],
+        render_kw={'class': 'form-control form-control-lg', 'placeholder': 'Email'})
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=6)],
+        render_kw={'class': 'form-control form-control-lg', 'placeholder': 'Password'})
+    submit = SubmitField(
+        "Login",
+        render_kw={'class': 'register_input'})
+    def validate_password(self,password,):
+        user = UserInfo.query.filter_by(email=self.email.data).first()
+        if user:
+            if not bcrypt.checkpw(password.data.encode('utf-8'),user.password.encode('utf-8')):
+                raise ValidationError('Incorrect Username or Password')
+        else:
+            raise ValidationError('Incorrect Username or Password')
 
 @app.route('/')
 def home():  # put application's code here
@@ -54,26 +81,55 @@ def home():  # put application's code here
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     from flask import request
+    business_type_opts = BusinessType.query.all()
+    choices = [
+
+    ]
     form = create_user_form()
-    debug = True
+
+    for opt in business_type_opts:
+        choices.append((opt.id,opt.description))
+    form.business_type.choices = choices
     if form.validate_on_submit():
+        business_type = form.business_type.data
+        business_name = form.business_name.data
         passwd = flask_bcrypt.generate_password_hash(form.password.data)
+        business = Business(name=form.business_name.data,
+                            type=form.business_type.data,
+                            phone=None,
+                            email=form.email.data,
+                            description=None)
+        db.session.add(business)
+        db.session.commit()
+        business_id = db.engine.execute(f'select id from business where name in ("{form.business_name.data}"); ').fetchone()
+        print(business_id['id'])
         user = UserInfo(first_name=form.first_name.data,
                         last_name=form.last_name.data,
-                        username=form.username.data,
-                        password=passwd)
+                        email=form.email.data,
+                        password=passwd,
+                        business=business_id['id'])
         db.session.add(user)
         db.session.commit()
-        debug = True
         return redirect('/')
     else:
         flash(form.errors)
-    return render_template('register.html', form=form)
 
+    return render_template('register.html', form=form)
+@login_manager.user_loader
+def load_user(id):
+    return UserInfo.query.filter_by(id=id).first()
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form=loginForm()
+    if form.validate_on_submit():
+        email=form.email.data
+        password=form.password.data
+        user = UserInfo.query.filter_by(email=email).first()
+        debug=True
+        login_user(user)
+        return render_template('index.html')
+    return render_template('login.html',form=form)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,14 +1,55 @@
 from __init__ import db
 
+
 class UserInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.VARCHAR(45))
     last_name = db.Column(db.VARCHAR(45))
-    username = db.Column(db.VARCHAR(45), nullable=False)
+    email = db.Column(db.VARCHAR(45), nullable=False)
     password = db.Column(db.VARCHAR(300), nullable=False)
+    business = db.Column(db.Integer)
 
-    def __init__(self, first_name, last_name,username, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.username = username
-        self.password = password
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
+    def __init__(self, first_name, last_name, email, password, business):
+        self.first_name = first_name or None
+        self.last_name = last_name or None
+        self.email = email or None
+        self.password = password or None
+        self.business = business or None
+
+
+class BusinessType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.VARCHAR)
+    description = db.Column(db.VARCHAR)
+
+    def __init__(self, title, description):
+        self.title = title or None
+        self.description = description or None
+
+
+class Business(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.VARCHAR)
+    phone = db.Column(db.VARCHAR)
+    email = db.Column(db.VARCHAR)
+    description = db.Column(db.VARCHAR)
+    type = db.Column(db.VARCHAR)
+
+    def __init__(self, name, phone, email, description, type):
+        self.name = name or None
+        self.phone = phone or None
+        self.email = email or None
+        self.description = description or None
+        self.type = type or None
