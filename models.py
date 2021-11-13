88254ff1c8a9fdf976213318738c8a4383e5bad2
Andrew Marxer <andy.marxer@yahoo.com>
2021-11-13 00:42:13 -0600

    add dropdown for user navigation(profile, logout, anythin else we will need).

--- models.py
+++ models.py
@@ -55,3 +55,43 @@ class Business(db.Model):
         self.email = email or None
         self.description = description or None
         self.type = type or None
+
+
+class UserProfile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    business = db.Column(db.Integer)
+    user_description = db.Column(db.VARCHAR)
+    salary = db.Column(db.Integer)
+    pay_frequency = db.Column(db.VARCHAR)
+    experience_years = db.Column(db.Integer)
+    employment_type = db.Column(db.VARCHAR)
+    phone = db.Column(db.VARCHAR)
+    city = db.Column(db.VARCHAR)
+    state = db.Column(db.VARCHAR)
+    twitter_link = db.Column(db.VARCHAR)
+    facebook_link = db.Column(db.VARCHAR)
+    website_link = db.Column(db.VARCHAR)
+    skills_description = db.Column(db.VARCHAR)
+    profile_picture_path = db.Column(db.VARCHAR)
+
+    def __init__(self, id, user_id, business, user_description, salary, pay_frequency, experience_years,
+                 employment_type,
+                 phone, city, state, twitter_link, facebook_link, website_link, skills_description,
+                 profile_picture_path):
+        self.id = id or None
+        self.user_id = user_id or None
+        self.business = business or None
+        self.user_description = user_description or None
+        self.salary = salary or None
+        self.pay_frequency = pay_frequency or None
+        self.experience_years = experience_years or None
+        self.employment_type = employment_type or None
+        self.phone = phone or None
+        self.city = city or None
+        self.state = state or None
+        self.twitter_link = twitter_link or None
+        self.facebook_link = facebook_link or None
+        self.website_link = website_link or None
+        self.skills_description = skills_description or None
+        self.profile_picture_path = profile_picture_path or None
