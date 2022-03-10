027f8935aa986e3da6c58b0a9815468bf7afb7b8
bikramdahal59 <bikramdahal@my.unt.edu>
2022-03-10 22:07:04 +0545

    Add test for posting of hiring of contractor by customer

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,14 +1,14 @@
 import re
 
 import pytest
-from models import UserInfo, Jobs
+from models import UserInfo, Jobs, Projects
 
 
 @pytest.mark.usefixtures('logged_in_user')
-def test_add_task(client, flask_app, auth):
+def test_add_task(client, flask_app):
     """
     GIVEN a Flask application
-    WHEN the '/register' page is requested (GET)
+    WHEN the '/test_add_task' page is requested (Post)
     THEN check the response is valid
     """
 
@@ -16,7 +16,7 @@ def test_add_task(client, flask_app, auth):
     response = client.get('/dashboard')
     assert response.status_code == 200
 
-    register_data = {
+    task_data = {
         'task_name': 'foo',
         'eventstarttime': '00:00',
         'eventstartdate': '2022-02-22',
@@ -28,7 +28,7 @@ def test_add_task(client, flask_app, auth):
     }
     response = client.post(
         '/addTask',
-        data=register_data,
+        data=task_data,
     )
     # Check the response is valid and redirects to the dashboard
     assert response.headers['Location'] == 'http://localhost/dashboard'
@@ -36,7 +36,34 @@ def test_add_task(client, flask_app, auth):
     with flask_app.app_context():
         job = Jobs.query.first()
         assert job is not None
-        assert job.job_title == register_data['task_name']
-        assert job.progress == register_data['progress']
-        assert job.user_id == register_data['user_id']
-        assert job.color == register_data['exampleColorInput']
+        assert job.job_title == task_data['task_name']
+        assert job.progress == task_data['progress']
+        assert job.user_id == task_data['user_id']
+        assert job.color == task_data['exampleColorInput']
+
+
+@pytest.mark.usefixtures('logged_in_user')
+def test_hire_post(client, flask_app):
+    # Ensure that the user is logged in
+    response = client.get('/customer_dashboard')
+    assert response.status_code == 200
+
+    hire_data = {
+        'contractor_id': 1,
+        'user_id': 1
+    }
+
+    response = client.post(
+        '/hire',
+        data=hire_data,
+    )
+    # Check the response is valid and redirects to the dashboard
+    assert response.headers['Location'] == 'http://localhost/customer_dashboard'
+
+    # Check for project properties
+    with flask_app.app_context():
+        project = Projects.query.first()
+        assert project is not None
+        assert project.business_id == hire_data['contractor_id']
+        assert project.user_id == hire_data['user_id']
+        assert project.is_approved is None
