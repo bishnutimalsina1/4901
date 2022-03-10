819dda1b65d00a2eb468f09647a7213866227853
bikramdahal59 <bikramdahal@my.unt.edu>
2022-03-10 22:21:17 +0545

    Add tests for approve workflow after hiring contractor user can approve it

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -67,3 +67,43 @@ def test_hire_post(client, flask_app):
         assert project.business_id == hire_data['contractor_id']
         assert project.user_id == hire_data['user_id']
         assert project.is_approved is None
+
+
+@pytest.mark.usefixtures('logged_in_user')
+def test_approve_post(client, flask_app):
+    # Ensure that the user is logged in
+    response = client.get('/approve')
+    assert response.status_code == 302
+    assert response.headers['Location'] == 'http://localhost/dashboard/projects'
+
+    hire_data = {
+        'contractor_id': 1,
+        'user_id': 1
+    }
+
+    response = client.post(
+        '/hire',
+        data=hire_data,
+    )
+    assert response.headers['Location'] == 'http://localhost/customer_dashboard'
+
+    approve_data = {
+        'contractor_id': 1,
+        'user_id': 1,
+        'project_id': 1,
+    }
+
+    response = client.post(
+        '/approve',
+        data=approve_data,
+    )
+    # Check the response is valid and redirects to the dashboard
+    assert response.headers['Location'] == 'http://localhost/dashboard/projects'
+
+    # Check for project properties
+    with flask_app.app_context():
+        project = Projects.query.first()
+        assert project is not None
+        assert project.user_id == hire_data['user_id']
+        # Project state should be approved
+        assert project.is_approved == 1
