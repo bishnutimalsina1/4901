2c50e2b622e42cadf0cefd9beb48f6ff5e2d1533
aman-me <amandhakal@my.unt.edu>
2022-04-14 22:42:49 +0545

    Update test for dashboard for bad http method and non-authenticated method

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -7,3 +7,11 @@ def test_dashboard(client):
     assert resp.status_code == 200
 
 
+def test_dashboard_bad_http_method(client):
+    resp = client.post('/dashboard')
+    assert resp.status_code == 405
+
+
+def test_dashboard_401_http_method(client):
+    resp = client.get('/dashboard')
+    assert resp.status_code == 401
